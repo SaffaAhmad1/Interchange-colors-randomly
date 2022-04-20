7178e5f435064b30f670397ee8019c36c02aec05
Saffa Tolba <safatolba984@gmail.com>
2022-04-20 16:28:33 +0200

    first coment

---
+++ main.js
@@ -0,0 +1,11 @@
+ function changeBg(){
+    let num1 = Math.floor(Math.random() * 300)
+    let num2 = Math.floor(Math.random() * 300)
+    let num3 = Math.floor(Math.random() * 300)
+    let num4 = Math.floor(Math.random() * 300)
+    let color = `rgb(${num1},${num2},${num3},${num4})`
+
+    document.querySelector("body").style.backgroundColor = color;
+
+
+}
